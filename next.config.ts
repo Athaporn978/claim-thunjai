03df973e989baac1d5b92142c4bda1a90014f979
Athaporn Kummaraka <arthur@MacBook-Air-khxng-Athaporn.local>
2026-08-04 08:34:03 +0700

    security: block direct static file downloads for .xlsx, .xls, .csv, .db in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*.xlsx",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/:path*.xls",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/:path*.csv",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/:path*.db",
+        destination: "/404",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
